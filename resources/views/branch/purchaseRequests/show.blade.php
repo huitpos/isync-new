75804ed4016a08246229f8833b86a3f0342e8f20
Raphael Villanueva <arvee1220@gmail.com>
2024-05-12 21:54:46 +0800

    POSADMIN-294

--- resources/views/branch/purchaseRequests/show.blade.php
+++ resources/views/branch/purchaseRequests/show.blade.php
@@ -81,7 +81,7 @@
                 <div class="row mb-5">
                     <div class="col-md-6">
                         <label class="form-label">Payment Terms</label>
-                        <select name="payment_term_id" id="" class="form-select @error('payment_term_id') is-invalid @enderror">
+                        <select {{ $pr->status != 'pending' ? 'disabled' : '' }} name="payment_term_id" id="" class="form-select @error('payment_term_id') is-invalid @enderror">
                             @foreach($company->paymentTerms as $paymentTerm)
                                 <option value="{{ $paymentTerm->id }}" {{ $pr->payment_term_id == $paymentTerm->id ? 'selected' : '' }}>{{ $paymentTerm->name }}</option>
                             @endforeach
@@ -94,7 +94,7 @@
 
                     <div class="col-md-6">
                         <label class="form-label">Supplier Terms</label>
-                        <select name="supplier_term_id" id="" class="form-select @error('supplier_term_id') is-invalid @enderror">
+                        <select {{ $pr->status != 'pending' ? 'disabled' : '' }} name="supplier_term_id" id="" class="form-select @error('supplier_term_id') is-invalid @enderror">
                             @foreach($company->supplierTerms as $supplierTerm)
                                 <option value="{{ $supplierTerm->id }}" {{ $pr->supplier_term_id == $supplierTerm->id ? 'selected' : '' }}>{{ $supplierTerm->name }}</option>
                             @endforeach
