269c618ddd091d3bd7ebab0252bfcbb4e38ce56e
Raphael Villanueva <arvee1220@gmail.com>
2024-06-07 15:27:13 +0800

    fixes

--- app/Http/Controllers/Admin/MachineController.php
+++ app/Http/Controllers/Admin/MachineController.php
@@ -99,8 +99,8 @@ class MachineController extends Controller
                 },
             ],
             'tin' => 'required',
-            'limit_amount' => ['required', 'numeric', 'regex:/^\d+(\.\d{1,4})?$/'],
-            'vat' => ['required', 'numeric', 'regex:/^\d+(\.\d{1,4})?$/'],
+            'limit_amount' => ['required', 'numeric', 'regex:/^-?\d+(\.\d{1,4})?$/'],
+            'vat' => ['required', 'numeric', 'regex:/^-?\d+(\.\d{1,4})?$/'],
         ]);
 
         if ($this->posMachineRepository->create($request->all())) {
@@ -204,8 +204,8 @@ class MachineController extends Controller
                 },
             ],
             'tin' => 'required',
-            'limit_amount' => ['required', 'numeric', 'regex:/^\d+(\.\d{1,4})?$/'],
-            'vat' => ['required', 'numeric', 'regex:/^\d+(\.\d{1,4})?$/'],
+            'limit_amount' => ['required', 'numeric', 'regex:/^-?\d+(\.\d{1,4})?$/'],
+            'vat' => ['required', 'numeric', 'regex:/^-?\d+(\.\d{1,4})?$/'],
         ]);
 
         if ($this->posMachineRepository->update($machineId, $request->all())) {
