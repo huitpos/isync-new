33f3bb22e477519b2e5e58af8e1145736e4e8780
Raphael Villanueva <arvee1220@gmail.com>
2024-06-10 08:50:33 +0800

    POSADMIN-395

--- resources/views/company/roles/index.blade.php
+++ resources/views/company/roles/index.blade.php
@@ -1,11 +1,7 @@
 <x-default-layout>
 
     @section('title')
-        Users
-    @endsection
-
-    @section('breadcrumbs')
-        {{ Breadcrumbs::render('company.users.index', $company) }}
+        Roles
     @endsection
 
     <div class="card">
