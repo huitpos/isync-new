32eda2546eff1f8dd477b0dba5bb07cdc02d88d4
Raphael Villanueva <arvee1220@gmail.com>
2024-06-19 22:29:22 +0800

    fixes

--- app/Http/Controllers/Api/MiscController.php
+++ app/Http/Controllers/Api/MiscController.php
@@ -1457,7 +1457,7 @@ class MiscController extends BaseController
             'transaction_id' => 'required',
             'discount_id' => 'required',
             'name' => 'required',
-            'value' => ['required', 'numeric', 'regex:/^-?\d+(\.\d{1,4})?$/'],
+            'value' => 'required',
             'is_cut_off' => 'required',
             'cut_off_id' => 'required',
             'is_void' => 'required',
