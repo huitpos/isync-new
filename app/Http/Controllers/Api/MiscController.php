58bd9ef753421196ba0e1d25c3ec1e12b717b08e
Raphael Villanueva <arvee1220@gmail.com>
2024-07-10 15:20:28 +0800

    include inactive products in api

--- app/Http/Controllers/Api/MiscController.php
+++ app/Http/Controllers/Api/MiscController.php
@@ -144,12 +144,10 @@ class MiscController extends BaseController
                     $query->where('updated_at', '>=', $request->from_date)
                           ->orWhere('created_at', '>=', $request->from_date);
                 })
-                ->where('status', 'active')
                 ->where('uom_id', '>', 0)
                 ->get();
         } else {
             $products = $branch->company->products()
-                ->where('status', 'active')
                 ->where('uom_id', '>', 0)
                 ->get();
         }
