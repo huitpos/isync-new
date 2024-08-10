91f256cab166b6d207ec951c0f7673f8a9126793
Raphael Villanueva <arvee1220@gmail.com>
2024-08-10 22:03:57 +0800

    daterange picker

--- resources/_keenthemes/src/js/components/app.js
+++ resources/_keenthemes/src/js/components/app.js
@@ -268,8 +268,8 @@ var KTApp = function () {
                     'Last 30 Days': [moment().subtract(29, 'days'), moment()],
                     'This Month': [moment().startOf('month'), moment().endOf('month')],
                     'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
+                    'Last Year': [moment().subtract(1, 'year').startOf('year'), moment().subtract(1, 'year').endOf('year')],
                     'Year to Date': [moment().startOf('year'), moment()],
-                    'Last Year': [moment().subtract(1, 'year').startOf('year'), moment().subtract(1, 'year').endOf('year')]
                 }
             }, cb);
     
