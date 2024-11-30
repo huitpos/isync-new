12f7793878bbd25716e4d70a0dfd1025a9a75a5b
Raphael Villanueva <arvee1220@gmail.com>
2024-11-30 13:43:28 +0800

    fixes

---
+++ database/migrations/transactional_db/2024_11_30_133953_alter_take_order_discount_other_informations_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('take_order_discount_other_informations', function (Blueprint $table) {
+            $table->unsignedBigInteger('company_id')->nullable();
+            $table->index('company_id', 'company_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('take_order_discount_other_informations', function (Blueprint $table) {
+            //
+        });
+    }
+};
