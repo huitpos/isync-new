3c1e8c9ea495e2c77a254cebae365cdddd16cd87
Raphael Villanueva <arvee1220@gmail.com>
2024-12-12 16:00:41 +0800

    add min and max column on the product template

--- app/Traits/CreatedUpdatedBy.php
+++ app/Traits/CreatedUpdatedBy.php
@@ -8,17 +8,17 @@ trait CreatedUpdatedBy
     {
         // updating created_by and updated_by when model is created
         static::creating(function ($model) {
-            if (!$model->isDirty('created_by')) {
+            if (!$model->isDirty('created_by') && auth()->user()) {
                 $model->created_by = auth()->user()->id;
             }
-            if (!$model->isDirty('updated_by')) {
+            if (!$model->isDirty('updated_by') && auth()->user()) {
                 $model->updated_by = auth()->user()->id;
             }
         });
 
         // updating updated_by when model is updated
         static::updating(function ($model) {
-            if (!$model->isDirty('updated_by')) {
+            if (!$model->isDirty('updated_by') && auth()->user()) {
                 $model->updated_by = auth()->user()->id;
             }
         });
