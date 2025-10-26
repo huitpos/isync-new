8accc9b38dfd6f409f905bb3b321a78a6acf3248
Raphael Villanueva <arvee1220@gmail.com>
2025-10-26 21:19:52 +0800

    fixes

--- app/Http/Controllers/Admin/ClientController.php
+++ app/Http/Controllers/Admin/ClientController.php
@@ -124,7 +124,8 @@ class ClientController extends Controller
             'barangay_id' => $request['barangay_id'],
             'slug' => Str::slug($request['trade_name']),
             'street' => $request['street'],
-            'company_registered_name' => ''
+            'company_registered_name' => '',
+            'pos_type' => $request['pos_type'],
         ]);
 
         $path = '';
@@ -239,6 +240,7 @@ class ClientController extends Controller
             'floor_number' => $request['unit_floor_number'],
             'city' => $request['city_id'],
             'province' => $request['province_id'],
+            'pos_type' => $request['pos_type'],
         ]);
 
         $path = '';
