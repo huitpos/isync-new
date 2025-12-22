4746fcc5d6c2056788b2cfe30df8d4b8064cd6f9
Raphael Villanueva <arvee1220@gmail.com>
2025-12-22 19:45:54 +0800

    raw material inventory is not being deducted when linked to a product that was sold

--- app/Http/Controllers/Api/MiscController.php
+++ app/Http/Controllers/Api/MiscController.php
@@ -1527,10 +1527,19 @@ class MiscController extends BaseController
 
                 if (isset($endOfDay['products'])) {
                     foreach ($endOfDay['products'] as $reqProduct) {
-                        $product = Product::find($reqProduct['productId']);
+                        $product = Product::find($reqProduct['productId'])
+                            ->load('bundledItems', 'rawItems');
 
                         if ($product) {
                             $this->productRepository->updateBranchQuantity($product, $branch, $reqProduct['endOfDayId'], 'end_of_days', $reqProduct['qty'], null, 'subtract', $product->uom_id);
+
+                            foreach ($product->bundledItems as $bundledItem) {
+                                $this->productRepository->updateBranchQuantity($bundledItem, $branch, $reqProduct['endOfDayId'], 'end_of_days', $reqProduct['qty'] * $bundledItem->bundled_item->quantity, null, 'subtract', $bundledItem->uom_id);
+                            }
+
+                            foreach ($product->rawItems as $rawItem) {
+                                $this->productRepository->updateBranchQuantity($rawItem, $branch, $reqProduct['endOfDayId'], 'end_of_days', $reqProduct['qty'] * $rawItem->bundled_item->quantity, null, 'subtract', $rawItem->uom_id);
+                            }
                         }
                     }
                 }
