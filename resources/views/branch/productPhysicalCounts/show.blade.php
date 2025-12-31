858869f25398f69f51389894f8adc710b565a6d7
Raphael Villanueva <arvee1220@gmail.com>
2025-12-31 11:13:33 +0800

    approver name

--- resources/views/branch/productPhysicalCounts/show.blade.php
+++ resources/views/branch/productPhysicalCounts/show.blade.php
@@ -25,6 +25,11 @@
                         <label class="form-label">Approved/Rejected By</label>
                         <input value="{{ ucfirst($count->actionBy?->name) }}" type="text" readonly class="form-control"/>
                     </div>
+
+                    <div class="col-md-6">
+                        <label class="form-label">Approved/Rejected Date</label>
+                        <input value="{{ $count->action_date }}" type="text" readonly class="form-control"/>
+                    </div>
                     @endif
                 </div>
 
--- resources/views/branch/purchaseRequests/show.blade.php
+++ resources/views/branch/purchaseRequests/show.blade.php
@@ -28,9 +28,14 @@
 
                 @if ($pr->status != 'pending')
                 <div class="row mb-5">
-                    <div class="col-md-12">
+                    <div class="col-md-6">
                         <label class="form-label">Approved/Rejected By:</label>
                         <input value="{{ $pr->actionBy->name }}" type="text" readonly class="form-control"/>
+                    </div>
+
+                    <div class="col-md-6">
+                        <label class="form-label">Approved/Rejected Date:</label>
+                        <input value="{{ $pr->action_date }}" type="text" readonly class="form-control"/>
                     </div>
                 </div>
                 @endif
